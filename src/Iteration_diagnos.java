a7812e35c1cc71a156af2676bbdb0cda161e303d
Snubb <60389050+Snubb@users.noreply.github.com>
2020-10-23 12:57:37 +0200

    Frågar nu om man vill köra igen. Ändrade bara på while versionen eftersom jag är lat.

--- src/Iteration_diagnos.java
+++ src/Iteration_diagnos.java
@@ -2,11 +2,16 @@ import javax.swing.*;
 
 public class Iteration_diagnos {
     public static void main(String[] args) {
-        int i = 1;
-        int tal = Integer.parseInt(JOptionPane.showInputDialog("Vilket tal?"));
-        while (i < 11) {
-            System.out.println(i + "*" + tal + "=" + i * tal);
-            i++;
+        String igen = "ja";
+        while (igen.equals("ja")) {
+            int i = 1;
+            int tal = Integer.parseInt(JOptionPane.showInputDialog("Vilket tal?"));
+            while (i < 11) {
+                System.out.println(i + "*" + tal + "=" + i * tal);
+                i++;
+            }
+            igen = JOptionPane.showInputDialog("Igen?(ja/nej)");
+            igen = igen.toLowerCase();
         }
     }
 }
